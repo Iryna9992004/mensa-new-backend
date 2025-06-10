93ee8081d451d1dab9f6cb10d737e31764f92789
Iryna9992004 <iren171302@gmail.com>
2025-06-10 15:52:10 +0300

    tweak eslint

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import express, { Application } from "express";
 import dotenv from "dotenv";
+import express, { Application } from "express";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 const app: Application = express();
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT.toString()}`);
 });
